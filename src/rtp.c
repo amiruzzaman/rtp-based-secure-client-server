533cd5d9b3337545348aff9009a6d3c70a8ba2b1
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-09-25 17:50:25 -0400

    wip: deserialization, testing
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- src/rtp.c
+++ src/rtp.c
@@ -10,17 +10,20 @@
             goto defer;                                                        \
         }                                                                      \
     } while (0)
-#define BOOL_FLAG(bool_val) (bool_val ? 1 : 0)
+#define BOOL_FLAG(bool_val) ((bool_val) ? 1 : 0)
+
+#define REINTERPRET_CAST(type, lvalue) \
+    ((type*)(lvalue))
 
 static size_t rtp_header_serialize(const struct rtp_header *restrict header,
                                    size_t bufflen,
-                                   uint8_t buff[RTP_UTIL_BITS_TO_BYTES(bufflen)]);
+                                   uint8_t buff[restrict bufflen]);
 
 size_t rtp_header_size(const struct rtp_header *header) {
     // 12 octets at least, and then a number of 32-bit CSRCs, and then
     // optionally the extension.
-    return 12 * 8 + header->csrc_count * 32 +
-           ((header->has_extension) ? header->ext.ext_len : 0);
+    return 12 + header->csrc_count * 4 +
+           ((header->has_extension) ? header->ext.ext_len * 4 : 0);
 }
 
 size_t rtp_packet_size(const struct rtp_packet *packet) {
@@ -29,9 +32,8 @@ size_t rtp_packet_size(const struct rtp_packet *packet) {
 
 size_t rtp_packet_serialize(const struct rtp_packet *restrict packet,
                             size_t bufflen,
-                            uint8_t buff[RTP_UTIL_BITS_TO_BYTES(bufflen)]) {
-    size_t pack_size = rtp_packet_size(packet);
-    CHECK_BUFF_LEN(bufflen, pack_size);
+                            uint8_t buff[restrict bufflen]) {
+    CHECK_BUFF_LEN(bufflen, rtp_packet_size(packet));
 
     size_t fill_len = rtp_header_serialize(&packet->header, bufflen, buff);
     size_t byte_data_len = RTP_UTIL_BITS_TO_BYTES(packet->payload.data_len);
@@ -48,20 +50,57 @@ defer:
 
 static size_t rtp_header_serialize(const struct rtp_header *restrict header,
                                    size_t bufflen,
-                                   uint8_t buff[RTP_UTIL_BITS_TO_BYTES(bufflen)]) {
+                                   uint8_t buff[restrict bufflen]) {
     size_t fill_len = 0;
-    // first octet
+    // 1st octet
     buff[fill_len] = header->csrc_count + (BOOL_FLAG(header->has_extension) << 4) +
               (BOOL_FLAG(header->has_padding) << 5) + (header->version << 6);
     ++fill_len;
 
-    // second octet
+    // 2nd octet
     buff[fill_len] = header->payload_type + (BOOL_FLAG(header->marker) << 7);
     ++fill_len;
 
-    // sequence number
-    // TODO
+    // 3th and 4th octets, sequence number
+    *REINTERPRET_CAST(uint16_t, buff + fill_len) = header->seq_num;
+    fill_len += sizeof(uint16_t);
 
-defer:
+    // 5th to 8th octets, timestamp
+    *REINTERPRET_CAST(uint32_t, buff + fill_len) = header->timestamp;
+    fill_len += sizeof(uint32_t);
+
+    // 9th to 12th octets, SSRC
+    *REINTERPRET_CAST(uint32_t, buff + fill_len) = header->ssrc;
+    fill_len += sizeof(uint32_t);
+
+    // some amount of 4-octets more, CSRC list
+    for(uint8_t csrc = 0; csrc < header->csrc_count;
+            fill_len += sizeof(uint32_t), ++csrc) {
+        *REINTERPRET_CAST(uint32_t, buff + fill_len) = header->csrcs[csrc];
+    }
+
+    // if there's extension, fill that in also
+    if(header->has_extension) {
+        // 2 octets profile id, 2 octets extention length
+        *REINTERPRET_CAST(uint16_t, buff + fill_len) = header->ext.profile_id;
+        fill_len += sizeof(uint16_t);
+        *REINTERPRET_CAST(uint16_t, buff + fill_len) = header->ext.ext_len;
+        fill_len += sizeof(uint16_t);
+        // fill in the extension
+        for(size_t ext_idx = 0; ext_idx < header->ext.ext_len - 1;
+                ++ext_idx, fill_len += 4) {
+            *REINTERPRET_CAST(uint32_t, buff + fill_len) = header->ext.ext[ext_idx];
+        }
+    }
+
     return fill_len;
+}
+
+// TODO: deserialization.
+
+size_t rtp_packet_deserialize(struct rtp_packet *restrict packet,
+                              size_t bufflen,
+                              uint8_t buff[restrict bufflen]) {
+    size_t pre_ext_header_size = rtp_header_deserialize_pre_ext(&packet->header, bufflen, buff);
+    // TODO
 }
--- src/rtp.h
+++ src/rtp.h
@@ -9,9 +9,9 @@
 
 struct rtp_ext {
     uint16_t profile_id;
-    // length in bits
+    // length in 4-octet words, including the 4-octet extension header
     uint16_t ext_len;
-    uint8_t *ext;
+    uint32_t *ext;
 };
 // TODO: since we have to serialize the packet before sending anyways, why not
 // make the header more readable? Y'know, like separating the first octet into
@@ -75,20 +75,20 @@ struct rtp_header {
      * 0 to 15 CSRC sources. The number of CSRCs is CC (in our case,
      * csrc_count).
      */
-    uint32_t *csrcs;
+    uint32_t csrcs[15];
     /**
      * Might have some extensions.
      */
     struct rtp_ext ext;
 };
 /**
- * @brief The total size of the header in bits, including extension if any.
+ * @brief The total size of the header in bytes, including extension if any.
  * @warn Do not pass in a null pointer.
  */
 size_t rtp_header_size(const struct rtp_header *header);
 
 struct rtp_payload {
-    // data length in bits
+    // data length in bytes
     size_t data_len;
     uint8_t *data;
 };
@@ -97,18 +97,59 @@ struct rtp_packet {
     struct rtp_payload payload;
 };
 /**
- * @brief Total sie of the packet in bits
+ * @brief Total sie of the packet in bytes
  */
 size_t rtp_packet_size(const struct rtp_packet *packet);
 /**
  * @brief Serialize the packet into the provided buffer.
  * @warn Do not pass in a null pointer as packet.
- * @return The size filled in.
+ * @return The size filled in, in bytes
  *
  * If there's not enough room, nothing is filled in at all, and 0 is returned.
  */
 size_t rtp_packet_serialize(const struct rtp_packet *restrict packet,
                              size_t bufflen,
-                             uint8_t buff[RTP_UTIL_BITS_TO_BYTES(bufflen)]);
+                             uint8_t buff[restrict bufflen]);
 
+/**
+ * @warn Might call malloc; `packet->ext.ext` will need to be freed if that's the
+ * case. If `header->has_extension` then it needs to be freed.
+ * @return The size filled in
+ * 
+ * If `buff` doesn't contain enough data (e.g. not long enough for all the
+ * header components), 0 is returned.
+ */
+size_t rtp_packet_deserialize(struct rtp_packet *restrict packet,
+                              size_t bufflen,
+                              uint8_t buff[restrict bufflen]);
+
+// If you want fine-grained control of where the packet gets deserialized,
+// assemble the thing using the 4 functions below
+
+/**
+ * @brief Deserialize the packet up until the extension header (if any).
+ *
+ * This is useful to get information about the size of CSRC list and extension
+ * header, so that one can prepare a buffer large enough.
+ */
+size_t rtp_header_deserialize_pre_ext(struct rtp_header *restrict header,
+                            size_t bufflen,
+                            const uint8_t buff[restrict bufflen]);
+/**
+ * @brief Deserialize the fixed-size header extension.
+ * @param trunc_buff truncates the pre-extension part of the header.
+ * @warn Check if there's an extension first, `header->has_extension`.
+ */
+size_t rtp_header_deserialize_extension_header(struct rtp_header *restrict header,
+                            size_t bufflen,
+                            const uint8_t trunc_buff[restrict bufflen]);
+/**
+ * @brief Deserialize the variable-size header extension data.
+ * @param trunc_buff truncates the pre-extension part of the header, and the
+ * 4-octet header extension *header*.
+ * @warn Check the size of the header extension data, `header->ext.ext_len`.
+ */
+size_t rtp_header_deserialize_extension_data(struct rtp_header *restrict header,
+                            size_t bufflen,
+                            const uint8_t trunc_buff[restrict bufflen]);
 #endif
