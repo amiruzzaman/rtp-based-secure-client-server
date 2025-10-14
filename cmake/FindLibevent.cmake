d9a37e7fdc01bb95e85d6039fd7a86cd096b34c3
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-10-14 03:06:07 -0400

    fix(cmake): conan and native package interop
    I made a find module for libevent. The modules conan used advises using
    targets in the form `libevent::<component>`, which is technically the
    modern way to do it in cmake.
    cmake doesn't ship with a FindLibevent module.
    No testing has beem done on devices without pkg-config and/or devices
    that want to specify a custom library path.
    
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

---
+++ cmake/FindLibevent.cmake
@@ -0,0 +1,93 @@
+# .rst:
+# FindLibevent
+# -------------
+#
+# Find libevent.
+#
+# Imported Targets
+# ^^^^^^^^^^^^^^^^
+#
+# ``libevent::libevent``
+# ``libevent::core``
+# ``libevent::extra``
+# ``libevent::openssl``
+# ``libevent::pthread`` (if not on Windows)
+
+# mostly copy from CMake's shipped FindSQLite3 module
+
+function(__find_libevent_create_import_target _target _import_loc)
+    if(NOT TARGET ${_target})
+        add_library(${_target} UNKNOWN IMPORTED)
+        set_target_properties(${_target} PROPERTIES
+            IMPORTED_LOCATION "${${_import_loc}}")
+        target_include_directories(${_target}
+            INTERFACE
+            "${Libevent_openssl_INCLUDE_DIR}")
+    endif()
+endfunction()
+
+macro(__find_libevent_find_component_library _component_var _component_name _dir_hint)
+    find_library(${_component_var}
+             NAMES
+             ${_component_name}
+             HINTS
+             ${_dir_hint})
+    mark_as_advanced(${_component_var})
+endmacro()
+
+find_package(PkgConfig QUIET)
+if(PkgConfig_FOUND)
+    pkg_check_modules(PC_Libevent QUIET libevent)
+    pkg_check_modules(PC_Libevent_core QUIET libevent_core)
+    pkg_check_modules(PC_Libevent_extra QUIET libevent_extra)
+    pkg_check_modules(PC_Libevent_openssl QUIET libevent_openssl)
+    pkg_check_modules(PC_Libevent_pthread QUIET libevent_pthread)
+endif()
+
+find_path(Libevent_INCLUDE_DIR
+         NAMES
+         event2/event.h
+         HINTS
+         ${PC_Libevent_INCLUDE_DIRS})
+mark_as_advanced(Libevent_INCLUDE_DIR)
+
+__find_libevent_find_component_library(Libevent_LIBRARY event PC_Libevent_LIBRARY_DIRS)
+__find_libevent_find_component_library(Libevent_core_LIBRARY event_core PC_Libevent_LIBRARY_DIRS)
+__find_libevent_find_component_library(Libevent_extra_LIBRARY event_extra PC_Libevent_LIBRARY_DIRS)
+__find_libevent_find_component_library(Libevent_openssl_LIBRARY event_openssl PC_Libevent_LIBRARY_DIRS)
+if(NOT WIN32)
+    __find_libevent_find_component_library(Libevent_pthread_LIBRARY event_pthread PC_Libevent_LIBRARY_DIRS)
+endif()
+
+# get version
+if(Libevent_INCLUDE_DIR)
+    if(PC_Libevent_VERSION)
+        # the easy way
+        set(Libevent_VERSION ${PC_Libevent_VERSION})
+    else()
+        # and the hard way
+        file(STRINGS ${Libevent_INCLUDE_DIR}/event-config.h __ver_line
+             REGEX "^#define EVENT__PACKAGE_VERSION *\"[0-9]+\\.[0-9]+\\.[0-9]+\""
+             LIMIT_COUNT 1)
+        string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+"
+               Libevent_VERSION "${__ver_line}")
+        unset(__ver_line)
+    endif()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Libevent
+    REQUIRED_VARS Libevent_LIBRARY Libevent_core_LIBRARY Libevent_INCLUDE_DIR
+    VERSION_VAR Libevent_VERSION)
+
+# import target
+if(Libevent_FOUND)
+    set(Libevent_INCLUDE_DIRS ${Libevent_INCLUDE_DIR})
+    __find_libevent_create_import_target(libevent::libevent Libevent_LIBRARY)
+    __find_libevent_create_import_target(libevent::core Libevent_LIBRARY)
+    __find_libevent_create_import_target(libevent::extra Libevent_LIBRARY)
+    __find_libevent_create_import_target(libevent::openssl Libevent_LIBRARY)
+    if(NOT WIN32)
+        __find_libevent_create_import_target(libevent::pthreads Libevent_LIBRARY)
+    endif()
+endif()
