2fd839d3271a4bd52dbe0678cb78575668cf6575
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-11-11 11:01:39 -0500

    wip(rtp-session): still trying to see what info i store
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- src/rtp_session.h
+++ src/rtp_session.h
@@ -4,28 +4,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// TODO: the sending RTP session and receiving RTP session should be different.
+/**
+ * A participant, basically.
+ */
+struct rtp_src {
+    uint32_t ssrc;
+};
 
 /**
- * Each type of stream has its own RTP session.
+ * From RFC 3550: An association among a set of participants communicating with
+ * RTP.
  */
-struct rtp_session {
-    // actual number of packets received.
-    uint64_t n_pack_received;
-    // since sequence number is only 16 bits long, there's real chance it will
-    // wrap around. When it does, increment this number.
-    // So, the sequence number we use to compare packets' sequence numbers
-    // should be (n_shifted_seq_cycles << 16) + sequence number
-    uint32_t n_shifted_seq_cycles;
-    // this synchronization source.
-    uint32_t ssrc;
-    // timestamp of the last package sent/received.
-    uint32_t timestamp;
-    // default timestamp increment.
-    uint32_t timestamp_inc;
-    // max sequence number sent/received.
-    uint16_t max_seq_num;
-    uint8_t payload_type;
-};
+struct rtp_session {};
 
 #endif
